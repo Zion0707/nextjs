2e6981cd72ad92b6c8134f5cb38d3573e2f146e6
huangzihao <346445684@qq.com>
2020-08-13 17:12:20 +0800

    解决antd按需加载css无法解决问题解决

--- next.config.js
+++ next.config.js
@@ -1,10 +1,36 @@
-// css跟less 并存使用
-const withCSS = require('@zeit/next-css');
+// antd 按需加载 css 无法正常解析，可以参考这个里的配置 https://github.com/vercel/next.js/issues/8054#issuecomment-596476753
 const withLess = require('@zeit/next-less');
 
-// 必须添加这个才能正常引入antd的css，不然会报 ^= css标识符错误
-if (typeof require !== 'undefined') {
-    require.extensions['.css'] = (file) => {};
-}
+module.exports = () => {
+    return withLess({
+        lessLoaderOptions: {
+            modifyVars: {
+                'primary-color': '#066',
+            },
+            javascriptEnabled: true,
+        },
+        webpack: (config, { isServer }) => {
+            if (isServer) {
+                const antStyles = /antd\/.*?\/style.*?/;
+                const origExternals = [...config.externals];
+                config.externals = [
+                    (context, request, callback) => {
+                        if (request.match(antStyles)) return callback();
+                        if (typeof origExternals[0] === 'function') {
+                            origExternals[0](context, request, callback);
+                        } else {
+                            callback();
+                        }
+                    },
+                    ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+                ];
 
-module.exports = withLess(withCSS({}));
+                config.module.rules.unshift({
+                    test: antStyles,
+                    use: 'null-loader',
+                });
+            }
+            return config;
+        },
+    });
+};
