21bbbe230f44273616e65b625e77b16ec2aee8d9
huangzihao <346445684@qq.com>
2020-08-13 17:30:07 +0800

    add images

--- pages/project.js
+++ pages/project.js
@@ -3,6 +3,7 @@ export default function Project() {
     return (
         <div className="page project-page">
             <h4>Project</h4>
+            <img src="/images/01.png" />
             <Link href="/">go home</Link>
         </div>
     );
